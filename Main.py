4873b1984366e581ac91ec155d408d1f86dc4262
IkeKap <1906011@wpsstudent.com>
2018-11-23 14:45:25 -0500

    Got it working with caviats
    For some reason, EXACTLY the first half of the data exported is nonsense.
    For that reason, currently this code cannot do a run of length 1 million
       because it would overflow excels row limit.
    Currently I'm going to try concatenating a copy for use in excel export

--- Main.py
+++ Main.py
@@ -2,9 +2,9 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import math as m
 import xlsxwriter as xlsx
 import time as time # to show time to calculate
+import pandas as pd
 
 # input total number of random points
 total_random_points = int(input("\nNumber of random points for Monte Carlo estimate of Pi?\n>"))
@@ -13,7 +13,7 @@ total_random_points = int(input("\nNumber of random points for Monte Carlo estim
 start_time = time.time()
 
 # name for new workbook. Convention will be (Trial length)Pi(Trial Number)
-name = str(total_random_points) + 'Pi1'
+name = str(total_random_points) + 'Pi' + str(1) + '.xlsx'
 # setting up an Excel workbook for the project
 workbook = xlsx.Workbook(name)
 worksheet = workbook.add_worksheet()
@@ -61,6 +61,30 @@ print (f"Difference to exact value of pi: {pi_approx-np.pi}")
 print (f"Percent Error: (approx-exact)/exact*100: {(pi_approx-np.pi)/np.pi*100}%")
 print (f"Execution Time: {time.time() - start_time} seconds\n")
 
+
+  # setting up the excel export
+
+worksheet.write_string(0,0, 'X values')
+worksheet.write_string(0,1, 'Y values')
+
+#creating a 2d array to make it easier to export to excel
+combined = np.vstack((x_plot_array,y_plot_array))
+
+
+row =1
+for col, data in enumerate(combined):
+    worksheet.write_column(row,col,data) 
+
+worksheet.write(0,5,'Approximate value for pi:')
+worksheet.write(0,6,pi_approx)
+worksheet.write(1,5,'Difference to exact value of pi:')
+worksheet.write(1,6,pi_approx-np.pi)
+worksheet.write(2,5,'Percent Error: (approx-exact)/exact*100:')
+worksheet.write(2,6,(pi_approx-np.pi)/np.pi*100)
+worksheet.write(3,5,'Execution Time:')
+worksheet.write(3,6,str(time.time() - start_time)+' seconds')
+workbook.close()
+
 # plot output of random points and circle. 
 inside_circle_plot = plt.scatter(x_inside_plot_array,y_inside_plot_array, color='blue', s=.1)
 outside_circle_plot = plt.scatter(x_outside_plot_array,y_outside_plot_array, color='red', s=.1)
@@ -74,3 +98,4 @@ ax.add_artist(outside_circle_plot)
 ax.add_artist(circle_plot)
 
 plt.show()
+print('test')
